397638b0812ddac66b8f092065e9998128264b0d
RaidenDilan <raiden18@me.com>
2020-03-07 21:55:10 +0000

    Initial Commit

---
+++ 01-basics.rb
@@ -0,0 +1,17 @@
+print "Enter a value: "
+
+first_num = gets.chomp
+
+print "Enter another value: "
+
+second_num = gets.chomp
+
+puts first_num.to_s + " + " + second_num.to_s + " + " + (first_num + second_num).to_s
+
+
+# Arithmetic Operators
+puts "6 + 4 = " + (6+4).to_s
+puts "6 - 4 = " + (6-4).to_s
+puts "6 * 4 = " + (6*4).to_s
+puts "6 / 4 = " + (6/4).to_s
+puts "6 % 4 = " + (6%4).to_s
